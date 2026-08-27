d9271da518a77763d8adfa7b1d3ee6d242e8100a
piexplorerpi <alladallod@gmail.com>
2026-08-27 12:33:04 +0330

    Create roadmapData.js

---
+++ frontend/src/data/roadmapData.js
@@ -0,0 +1,21 @@
+export const roadmapData = [
+  {
+    number: '01',
+    icon: '🌱',    key: 'phase1', // کلید برای ترجمه
+  },
+  {
+    number: '02',
+    icon: '🗳️',
+    key: phase2',
+  },
+  {
+    number: '03',
+    icon: '📊'
+    key: 'phase3',
+  },
+  {
+    number: '04',
+    icon: '🌐',
+    ke: 'phase4',
+  },
+];
