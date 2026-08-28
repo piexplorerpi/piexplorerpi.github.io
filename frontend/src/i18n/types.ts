4c8f6bbe0f1b51d84266769905e5d9be6192c024
piexplorerpi <alladallod@gmail.com>
2026-08-28 13:19:39 +0330

    Update types.ts

--- frontend/src/i18n/types.ts
+++ frontend/src/i18n/types.ts
@@ -1,6 +1,8 @@
-// src/i18n/types.ts
+// frontend/src/i18n/types.ts
+
 export type SupportedLang = 'en' | 'fa' | 'ar' | 'tr' | 'zh';
 
+// این تایپ تضمین می‌کند که هر خط ترجمه حتماً باید تمام زبان‌ها را داشته باشد
 export type LocalizedString = {
   [key in SupportedLang]: string;
 };
