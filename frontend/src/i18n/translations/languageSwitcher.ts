5906196986c63a5247e2b54754d77ce595d5f164
piexplorerpi <alladallod@gmail.com>
2026-08-31 15:59:04 +0330

    Create languageSwitcher.ts

---
+++ frontend/src/i18n/translations/languageSwitcher.ts
@@ -0,0 +1,12 @@
+// src/i18n/translations/languageSwitcher.ts
+import { LocalizedString } from '../types';
+
+export const languageSwitcherTranslations = {
+  label: {
+    en: "Language",
+    fa: "زبان",
+    ar: "اللغة",
+    tr: "Dil",
+    zh: "语言"
+  }
+};
